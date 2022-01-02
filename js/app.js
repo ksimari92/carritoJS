1e9c3c033ffb755c22e6e4be37fe1f6af1837d22
Karen Simari <karensimari@live.com.ar>
2022-01-01 21:45:15 -0300

    Nuevos estilos seccion productos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 let productos = document.getElementById("cont-productos");
 let url = "/js/productos.json";
+let productosCarrito = [];
 
 async function dibujar() {
 
@@ -14,12 +15,47 @@ async function dibujar() {
               <p> <strong> Precio: </strong> U$D ${datos.precio} </p>
               <p> <strong> Descripcion: </strong> ${datos.descripcion}</p>
 
-             <button class="btn-comprar">Agregar</button>
+             <button class="btn-comprar" id="${datos.id}">Agregar</button>
              </div>
           </div>`
             }
-
         })
+
 }
 
 dibujar();
+
+
+async function obtenerProductos(url) {
+    let datos = await fetch(url);
+    let productos = await datos.json();
+
+    console.log(productos);
+    return productos;
+}
+
+obtenerProductos(url);
+
+function agregarAlCarrito() {
+    idProducto = e.target.attributes.dataId.nodeValue;
+    seleccionado = productos.find(el => el.id == idProducto);
+    productosSeleccionados.push(seleccionado);
+
+    console.log(idProducto = e.target.attributes.dataId.nodeValue)
+    console.log(productosSeleccionados);
+
+    let storage = [];
+
+    if (!localStorage.getItem("productos")) {
+        storage.push(producto);
+        localStorage.setItem("productos", JSON.stringify(storage));
+    } else {
+        storage = JSON.parse(localStorage.getItem("productos"));
+        storage.push(producto);
+        localStorage.setItem("productos", JSON.stringify(storage));
+    }
+}
+
+for (let i = 1; i >= 12; i++) {
+    document.querySelectorAll(i).addEventListener("click", agregarAlCarrito)
+}
