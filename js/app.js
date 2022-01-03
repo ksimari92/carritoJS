4793755dd516e214a7ab7efbaae86a2714d1eddb
Karen Simari <karensimari@live.com.ar>
2022-01-02 21:49:36 -0300

    Agrego funcionalidad en carrito de compras

--- js/app.js
+++ js/app.js
@@ -1,41 +1,70 @@
 let productos = document.getElementById("cont-productos");
-let url = "/js/productos.json";
 let productosCarrito = [];
 
-async function dibujar() {
 
-    await fetch(url)
-        .then(datos => datos.json())
-        .then(data => {
-            for (const datos of data) {
-                productos.innerHTML += `<div class="card">
+
+
+const cargarDatos = async() => {
+    try {
+        const res = await fetch('js/productos.json');
+        const data = await res.json();
+        dibujar(data);
+
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function dibujar(data) {
+    for (const datos of data) {
+        productos.innerHTML += `<div class="card">
                 <img src=${datos.imagen} alt="">
               <div class="info">
               <p> <strong> Nombre: </strong> ${datos.nombre} </p>
               <p> <strong> Precio: </strong> U$D ${datos.precio} </p>
               <p> <strong> Descripcion: </strong> ${datos.descripcion}</p>
 
-             <button class="btn-comprar" id="${datos.id}">Agregar</button>
+             <button class="btn-comprar" onclick="agregarProducto(this)" id="${datos.id}">Agregar</button>
              </div>
           </div>`
-            }
-        })
+
+
+    }
+}
+
+async function agregarProducto(comp) {
+    let datos = await cargarDatos()
+
+    let id = comp.id;
+
+    for (const data of datos) {
+        if (id == data.id) {
+
+            productosCarrito.push(Number(data.precio));
+            console.log(productosCarrito);
+        }
+    }
+
+    document.getElementById("btn-total").innerHTML = ` <img src="img/shopping-cart_icon-icons.com_72552.png" alt="icono carrito"> $` + sumaTotal(productosCarrito);
 
 }
 
-dibujar();
+function sumaTotal(productosCarrito) {
+    let totalPrecio = 0;
+
+    for (let i = 0; i < productosCarrito.length; i++) {
+
+        totalPrecio = productosCarrito[i] + totalPrecio;
+
+    }
+    console.log(totalPrecio)
+
+    return totalPrecio;
+}
 
 
-// async function obtenerProductos(url) {
-//     let datos = await fetch(url);
-//     let productos = await datos.json();
-
-//     console.log(productos);
-//     return productos;
-// }
-
-// obtenerProductos(url);
-
-// function agregarAlCarrito() {
-
-// }
+cargarDatos();
+document.getElementById("btn-total").addEventListener('click', () => {
+    document.getElementById("modal-des").innerHTML = "El total de su compra es: $" + sumaTotal(productosCarrito);
+})
