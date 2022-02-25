312f1543680ba280188b4dcbe7478607d383c3e2
Karen Simari <karensimari@live.com.ar>
2022-02-25 10:40:49 -0300

    Funcionalidad aplicar descuento

--- js/terminarCompra.js
+++ js/terminarCompra.js
@@ -18,6 +18,7 @@ const recuperarObjetos = () => {
     }
 }
 
+
 document.getElementById("btn-total").innerHTML = ` <img src="img/shopping-cart_icon-icons.com_72552.png" alt="icono carrito">  ` + localStorage.getItem('cantidadProductos');
 
 
@@ -29,8 +30,38 @@ console.log(total)
 
 btnVaciar.addEventListener("click", () => {
     localStorage.clear();
-    lista.innerHTML="";
-    totalCompra.innerHTML = `TOTAL = U$D 0`;
+    localStorage.setItem('precioTotal', 0);
+    lista.innerHTML = "";
+    totalCompra.innerHTML = `TOTAL = U$D ${localStorage.getItem("precioTotal")}`;
     document.getElementById("btn-total").innerHTML = ` <img src="img/shopping-cart_icon-icons.com_72552.png" alt="icono carrito">`;
 
 })
+
+
+//SECCION TENES DESCUENTO
+let cupon = document.getElementById('cupon');
+let descuento = document.getElementById("total-descuento");
+
+const aplicarDescuento = (cupon) => {
+    console.log("click descuento")
+    console.log(cupon.value)
+    if(localStorage.getItem('precioTotal') < 0 || localStorage.getItem('precioTotal') == null){
+        descuento.innerHTML= `<p style="color: red">¡DEBE SELECCIONAR ARTICULOS DE LA TIENDA!</p>`
+    }
+    if (cupon.value === "UNDESCUENTO") {
+        total = localStorage.getItem('precioTotal') - (total * 0.1);
+        descuento.innerHTML = `<br>  Su total con el descuento es ${total}`
+    } else if (cupon.value === "DOSDESCUENTO") {
+        total = localStorage.getItem('precioTotal') - (total * 0.15);
+        descuento.innerHTML = `<br>  Su total con el descuento es ${total}`
+    } else if (cupon.value === "SUPERDESCUENTO") {
+        total = localStorage.getItem('precioTotal') - (total * 0.5);
+        descuento.innerHTML = `<br>  Su total con el descuento es ${total}`
+    } else {
+        descuento.innerHTML = `<p style="color: red">¡LO SENTIMOS! EL CÓDIGO NO ES VÁLIDO</p>`
+    }
+}
+
+document.getElementById('descuento').addEventListener("click", function () {
+    aplicarDescuento(cupon)
+});
